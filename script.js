99a0bf96e937eed927e2367ad0d990a8b50539bc
AlanBoy08 <alanwarlimdi@gmail.com>
2025-01-15 11:30:18 +0400

    few

--- script.js
+++ script.js
@@ -530,6 +530,7 @@ function navigateRoomToRoom(startRoom, endRoom) {
     "R27": {x: 887, y: 152, corridor: "C1"},
     "R30": {x: 938, y: 152, corridor: "C1"},
     "R35": {x: 672, y: 152, corridor: "C1"},
+    "R36": {x: 720, y: 152, corridor: "C1"},
     "The Staircase":{x: 586, y: 133, corridor: "C4"}
   };
 
